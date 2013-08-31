e76bda3a502ea8ec547aff4893a90a94e4c7545c
Ben Slaughter <b.p.slaughter@gmail.com>
2013-08-31 18:19:46 +0100

    added create and read for posts

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Insight::Application.config.secret_key_base = '3380101ef599dd2e43b492b1e171bbc5231e382f403c979e24a2fa1e03d44b47878f09712708a04ecc399c58af9f046317e8f00f54e83f512b2927ea113ba965'
